b684e8c4f9f8be7bc9244ca783391bd716e66810
Ckbhatia <chetansain86@gmail.com>
2020-07-05 18:52:14 +0530

    fix: remove unnessary code

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -35,11 +35,6 @@ export default function SimpleModal(props) {
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = React.useState(getModalStyle);
-  // const [open, setOpen] = React.useState(false);
-
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
 
   const handleClose = () => {
     setOpen(false);
