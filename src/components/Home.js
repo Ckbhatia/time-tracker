843d7946864c8b32dc55eb1500e9a195ef3453c1
Ckbhatia <chetansain86@gmail.com>
2020-07-05 16:40:33 +0530

    feat: create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+const Home = (props) => {
+  return (
+    <main className="main-home">
+      <div className="main-container">
+        // TODO: Add components
+      </div>
+    </main>
+  )
+};
+
+export default Home;
