bc5e875c6d90799b13c69f9c649493b4b3f71786
Ckbhatia <chetansain86@gmail.com>
2020-07-05 20:58:42 +0530

    feat: add task show

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import CreateTask from "./CreateTask";
+import TaskShow from "./TaskShow";
 
 const Home = (props) => {
   return (
     <main className="main-home">
       <div className="main-container">
         <CreateTask />
+        <TaskShow />
       </div>
     </main>
   )
