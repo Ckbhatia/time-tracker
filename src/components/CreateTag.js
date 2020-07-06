53fe885a9fcbf36e36549d658a6147c080797a12
Ckbhatia <chetansain86@gmail.com>
2020-07-06 10:59:19 +0530

    fix: add update tag and current tag

--- src/components/CreateTag.js
+++ src/components/CreateTag.js
@@ -1,48 +1,51 @@
 import React, { useState } from "react";
 import gql from "graphql-tag";
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery } from "@apollo/react-hooks";
 import { AiFillPlayCircle, AiTwotoneStop } from "react-icons/ai";
 import { GoPlus } from "react-icons/go";
 import Modal from "./Modal";
 
 const GetTags = gql`
   query {
-  tags (order_by: {created_at: desc}) {
-    id
-    name
+    tags(order_by: { created_at: desc }) {
+      id
+      name
+    }
   }
-}
 `;
 
 const CreateTag = ({ updateTag, currentTag }) => {
-	const [open, setOpen] = useState(false);
-	const { loading, error, data} = useQuery(GetTags);
+  const [open, setOpen] = useState(false);
+  const { loading, error, data } = useQuery(GetTags);
 
   if (error) return `Error! ${error.message}`;
 
   return (
     <main className="main-create-tag">
       <div className="main-container">
-      <div>
-      	{open && <Modal open={open} setOpen={setOpen}/>}
-      </div>
+        <div>{open && <Modal open={open} setOpen={setOpen} />}</div>
         <div className="create-tag-container">
-          <select className="tag-select-menu" name="tag-menu" onChange={(e) => console.log(e.target.value)}>
-          	{currentTag && <option className="default-tag">
-              {currentTag}
-            </option>}
+          <select
+            className="tag-select-menu"
+            name="tag-menu"
+            onChange={(e) => updateTag(e.target.value)}
+          >
+            {currentTag && (
+              <option className="default-tag">
+                {currentTag && currentTag.name}
+              </option>
+            )}
             <option className="create-tag" onClick={() => setOpen(!open)}>
-          		+ Tag
-          	</option>
-          	{!loading && data.tags.map(tag => (
-          		<option key={tag.id} value={tag.name}>
-          			{tag.name}
-          		</option>
-          		)
-          	)}
+              + Tag
+            </option>
+            {!loading &&
+              data.tags.map((tag) => (
+                <option key={tag.id} value={tag.name}>
+                  {tag.name}
+                </option>
+              ))}
           </select>
         </div>
-
       </div>
     </main>
   );
--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -1,60 +1,59 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import gql from "graphql-tag";
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import { MdDelete } from "react-icons/md";
 import CreateTag from "./CreateTag";
 
 const SimplifyTime = (date) => {
 	// TODO: fix this
 	return new Date(date).toLocaleTimeString().substring(0, 10);
-} 
+};
 
 const GetTasks = gql`
-  query {
-	  tasks(limit: 10) {
-	  title
-	  id
-	  created_at
-	  start_time
-	  end_time
-	  tags {
-	    name
-	    id
-	  }
+	query {
+		tasks(limit: 10) {
+			title
+			id
+			created_at
+			start_time
+			end_time
+			tags {
+				name
+				id
+			}
+		}
 	}
-}
 `;
 
 const deleteOneTask = gql`
-  mutation($id: Int!) {
-  delete_tasks(where: {id: {_eq: $id}}) {
-    returning {
-      id
-      title
-    }
-    affected_rows
-  }
-}
+	mutation($id: Int!) {
+		delete_tasks(where: { id: { _eq: $id } }) {
+			returning {
+				id
+				title
+			}
+			affected_rows
+		}
+	}
 `;
 
-
 const TaskShow = () => {
-	
-	const { loading, error, data} = useQuery(GetTasks);
-  const [deleteAtask] = useMutation(deleteOneTask);
+	const { loading, error, data } = useQuery(GetTasks);
+	const [deleteAtask] = useMutation(deleteOneTask);
+	const [tag, updateTag] = useState(null);
 
- const submitTaskData = (id) => {
-    deleteAtask({
-      variables: {
-        id
-      },
-    });
-    // TODO: add refresh function call here
-  };
+	const submitTaskData = (id) => {
+		deleteAtask({
+			variables: {
+				id,
+			},
+		});
+		// TODO: add refresh function call here
+	};
 
-  const updateTitle = (e) => {
-  	// 
-  } 
+	const updateTitle = (e) => {
+		//
+	};
 
 	if (error) return `Error! ${error.message}`;
 
@@ -62,38 +61,49 @@ const TaskShow = () => {
 		<div className="task-list-main-container">
 			<div className="task-list-container">
 				<ul className="list-container">
-					{!loading && data.tasks.map(task => (
-						<li key={task.id} value={task.name}>
-							<div className="task-container">
-								<div className="task-input-container">
-									<input 
-										className="task-input" 
-										type="text" 
-										name="task-title" 
-										value={task.title} 
-										onChange={(e) => task.title += e.target.value} />
+					{!loading &&
+						data.tasks.map((task) => (
+							<li key={task.id} value={task.name}>
+								<div className="task-container">
+									<div className="task-input-container">
+										<input
+											className="task-input"
+											type="text"
+											name="task-title"
+											value={task.title}
+											onChange={(e) => (task.title += e.target.value)}
+										/>
+									</div>
+									<div className="tag-container">
+										{/*TODO: Pass tag data to createTag component*/}
+										<CreateTag
+											updateTag={updateTag}
+											currentTag={task.tags[0]}
+										/>
+									</div>
+									<div className="task-timer-container">
+										{/*TODO: Simplify the date to only time, four digit*/}
+										<span className="start-time">
+											{SimplifyTime(task.start_time)}
+										</span>
+										<span className="end-time">
+											{SimplifyTime(task.end_time)}
+										</span>
+									</div>
+									<div className="delete-btn-container">
+										<button
+											className="delete-btn"
+											onClick={() => submitTaskData(task.id)}
+										>
+											<MdDelete />
+										</button>
+									</div>
 								</div>
-								<div className="tag-container">
-									{/*TODO: Pass tag data to createTag component*/}
-									<CreateTag currentTag={task.tags[0]}/>
-								</div>
-								<div className="task-timer-container">
-								{/*TODO: Simplify the date to only time, four digit*/}
-									<span className="start-time">{SimplifyTime(task.start_time)}</span>
-									<span className="end-time">{SimplifyTime(task.end_time)}</span>
-								</div>
-								<div className="delete-btn-container">
-									<button className="delete-btn" onClick={() => submitTaskData(task.id)}>
-										<MdDelete />
-									</button>
-								</div>
-							</div>
-						</li>
-						)
-					)}
+							</li>
+						))}
 				</ul>
 			</div>
 		</div>
-		)
-}
+	);
+};
 export default TaskShow;
