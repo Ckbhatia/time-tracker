ec343394919e81893ebbaa41ce7608f679e0b86c
Ckbhatia <chetansain86@gmail.com>
2020-07-08 11:40:58 +0530

    feat: add shouldUpdate state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import CreateTask from "./CreateTask";
 import TaskShow from "./TaskShow";
 
 const Home = (props) => {
-  return (
-    <main className="main-home">
-      <div className="main-container">
-        <CreateTask />
-        <TaskShow />
-      </div>
-    </main>
-  )
+	const [shouldRefetch, udpateShouldRefetch] = useState(false);
+
+	return (
+		<main className="main-home">
+			<div className="main-container">
+				<CreateTask udpateShouldRefetch={udpateShouldRefetch} />
+				<TaskShow
+					shouldRefetch={shouldRefetch}
+					udpateShouldRefetch={udpateShouldRefetch}
+				/>
+			</div>
+		</main>
+	);
 };
 
 export default Home;
