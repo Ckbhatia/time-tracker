29675ab89a55cbe2c50118b8e516d7d457720b8b
Ckbhatia <chetansain86@gmail.com>
2022-02-19 21:54:22 +0530

    feat: create date time helpers

---
+++ src/utils/dateTime.js
@@ -0,0 +1,49 @@
+import moment from "moment";
+
+/* Simplifies the date to AM/PM time only
+ *	param {object}
+ *	return {string}
+ */
+ export const SimplifyTime = (date) => {
+	if (date) {
+		return moment.utc(date).local().format("h:mm A");
+	}
+};
+
+
+export const getCurrentTime = () => {
+	return moment.utc().format();
+};
+
+
+/**
+ * Takes object date time & optional boolean & returns time
+ * @param {object} startTime
+ * @param {object} endTime
+ * @returns
+ */
+export const getDurationTime = (startTime, endTime) => {
+  if ((!startTime || !endTime) && typeof startTime !== 'object') return null;
+
+  const res = moment(endTime).diff(moment(startTime));
+
+  let { days, hours, minutes, seconds } = moment.duration(res)._data;
+
+  if (days < 0) {
+    days = 0;
+  }
+
+  if (hours < 0) {
+    hours = 0;
+  }
+
+  if (minutes < 0) {
+    minutes = 0;
+  }
+
+  if (seconds < 0) {
+    seconds = 0;
+  }
+
+  return { days, hours, minutes, seconds };
+};
