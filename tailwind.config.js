067040c41b9ff95ecbf10ae8a84976ab52c1f1dc
Ckbhatia <chetansain86@gmail.com>
2022-02-19 21:53:18 +0530

    feat: add colors

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'black': '#000000',
+        'main-dark-cyan': '#1d272b',
+        'seren-setting': '#c5d2d9',
+        'black-pearl': '#12191d',
+      },
+    }
+  }
+}
