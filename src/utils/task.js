76b11d741674aa8a285c5c4bc507b08325413784
Ckbhatia <chetansain86@gmail.com>
2022-02-23 10:35:59 +0530

    feat: add count total hours

--- src/utils/task.js
+++ src/utils/task.js
@@ -1,17 +1,32 @@
 import moment from "moment";
+import { getDurationTime } from "./dateTime";
 
 export const tasksByTime = (data) => {
   if(data && data?.tasks?.length) {
     return data.tasks.reduce((acc, task) => {
       const startTime = moment.utc(task?.start_time).local().format('ll');
+      const { hours, minutes, seconds } = getDurationTime(task.start_time, task.end_time);
       if(!acc[startTime]) {
-        acc[startTime] = [task];
+        acc[startTime] = { tasks: [task], totalTime: { totalHours: hours, totalMinutes: minutes, totalSeconds: seconds }};
       } else {
-        acc[startTime].push(task);
+        let { totalHours, totalMinutes, totalSeconds } = acc[startTime]['totalTime'];
+        totalHours += hours;
+        totalMinutes += minutes;
+        totalSeconds += seconds;
+
+        if(totalSeconds > 59) {
+          totalMinutes +=  1;
+        }
+        if(totalMinutes > 59) {
+          totalHours +=  1;
+        }
+
+        acc[startTime]["tasks"].push(task);
+        acc[startTime]["totalTime"] = { totalHours, totalMinutes, totalSeconds };
       }
       return acc;
     }, {});
   }
-
+  
   return {};
 }
