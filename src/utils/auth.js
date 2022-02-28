26a797b4598979a35fe07e9cbbca19a6cd73d6f4
Ckbhatia <chetansain86@gmail.com>
2022-02-28 12:27:40 +0530

    feat: create log out func

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,12 @@
+import client from "../utils/client";
+
 export const getIsAuthenticated = () => {
   const userInfo = localStorage.getItem("userInfo");
-  return userInfo?.token !== null;
+  return userInfo !== null;
 }
+
+export const logOut = async () => {
+  await localStorage.removeItem("userInfo");
+  await client.clearStore();
+  return true;
+};
