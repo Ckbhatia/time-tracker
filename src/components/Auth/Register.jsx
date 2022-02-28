f3c32113f73ca103b59c4e592bc3e72fa1354966
Ckbhatia <chetansain86@gmail.com>
2022-02-28 10:48:14 +0530

    feat: create register page

---
+++ src/components/Auth/Register.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@apollo/react-hooks";
+import { register } from "../../service";
+import {
+  StyledContainer,
+  StyledExtraActionContainer,
+  StyledFormContainer,
+  StyledInput,
+  StyledInputContainer,
+  StyledLinkContainer,
+  StyledSection,
+  StyledSubmit,
+} from "./Styles";
+import { routes } from "../../constants/routes";
+
+const Register = () => {
+  const [formData, setFormData] = React.useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const [registerUser, { data, loading, error }] = useMutation(register);
+
+  React.useEffect(() => {
+    if (data) {
+      const token = data?.signup?.token;
+      if(token) {
+        localStorage.setItem("userInfo", JSON.stringify({token}));
+        navigate(routes.tracker)
+      }
+    }
+  }, [data, navigate]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Submit form data to the server
+    registerUser({
+      variables: {
+        name,
+        username,
+        email,
+        password,
+      },
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const { name, username, email, password } = formData;
+
+  const isEmpty = !name || !username || !email || !password;
+
+  return (
+    <StyledSection>
+      <StyledContainer>
+        <StyledFormContainer onSubmit={handleSubmit}>
+          <StyledInputContainer>
+            <StyledInput
+              name="name"
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={handleChange}
+              required
+            />
+          </StyledInputContainer>
+          <StyledInputContainer>
+            <StyledInput
+              name="username"
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={handleChange}
+              required
+            />
+          </StyledInputContainer>
+          <StyledInputContainer>
+            <StyledInput
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={handleChange}
+              required
+            />
+          </StyledInputContainer>
+          <StyledInputContainer>
+            <StyledInput
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={handleChange}
+              required
+            />
+          </StyledInputContainer>
+          <StyledInputContainer>
+            <StyledSubmit disabled={isEmpty} type="submit" value="Register" />
+          </StyledInputContainer>
+        </StyledFormContainer>
+        <StyledExtraActionContainer>
+          <StyledLinkContainer>
+            Already a member?
+            <Link to="/login">Sign in</Link>
+          </StyledLinkContainer>
+        </StyledExtraActionContainer>
+      </StyledContainer>
+    </StyledSection>
+  );
+};
+
+export default Register;
---
+++ src/components/Auth/Styles.jsx
@@ -0,0 +1,75 @@
+import tw, { styled } from "twin.macro";
+
+export const StyledSection = styled.section`
+  ${tw`flex flex-col items-center justify-center h-screen`}
+  background-color: #253236;
+`;
+
+export const StyledContainer = styled.div`
+  width: 350px;
+  background-color: var(--black-pearl);
+  border-radius: 2px;
+  box-shadow: 0 0 20px rgb(0 0 0 / 10%);
+`;
+
+export const StyledFormContainer = styled.form`
+  padding: 35px 25px 0 25px;
+`;
+
+export const StyledInputContainer = styled.div`
+  ${tw`mb-6`};
+`;
+
+export const StyledInput = styled.input`
+  ${tw`
+    w-full
+    rounded-md
+    border border-gray-700
+    py-3
+    px-5
+    outline-none
+    focus-visible:shadow-none
+    text-white
+  `}
+  background-color: var(--black-pearl);
+
+`;
+
+export const StyledSubmit = styled.input`
+  ${tw`
+    w-full
+    rounded-md
+    border border-gray-700
+    py-3
+    px-5
+    text-base text-white
+    cursor-pointer
+    hover:bg-opacity-90
+    transition
+    text-base text-white
+  `}
+  background-color: var(--main-dark-cyan);
+  transition: all 0.2s ease-in-out;
+  &:hover { 
+    background-color: #253235;
+  }
+
+  &:disabled { 
+    cursor: default;
+    background-color: #414e53;
+  }
+`
+
+export const StyledExtraActionContainer = styled.div`
+  text-align: center;
+`
+
+export const StyledLinkContainer = styled.span`
+  ${tw`text-base text-white pt-2 pb-6`}
+  display: inline-block;
+
+  a {
+    color: #3f00ff;
+    margin-left: 8px;
+  }
+`;
