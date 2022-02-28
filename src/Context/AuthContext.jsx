52d0389a39873a4958983fea109ec9de295a7375
Ckbhatia <chetansain86@gmail.com>
2022-02-28 12:27:27 +0530

    feat: create auth provider

---
+++ src/Context/AuthContext.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children, value }) => {
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
