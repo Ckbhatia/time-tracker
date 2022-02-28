bfd8c9aa3689a130d303e016e248a4e3892a7015
Ckbhatia <chetansain86@gmail.com>
2022-02-28 10:49:22 +0530

    feat: create getAuthToken

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,5 +1,12 @@
+import get from "lodash/get";
+
 export const getUserId = () => {
   // return localStorage.getItem('userId');
   // Get and return original id of user
   return 3;
+}
+
+export const getAuthToken = async () => {
+  const userInfo = await JSON.parse(localStorage.getItem('userInfo'));
+  return get(userInfo, 'token');
 }
