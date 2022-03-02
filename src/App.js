1ca4d758922411174538335b8b996e389a171af3
Ckbhatia <chetansain86@gmail.com>
2022-03-02 17:56:36 +0530

    feat: add userInfo state

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./utils/client";
 import authClient from "./utils/auth_client";
 import { getIsAuthenticated } from "./utils/auth";
+import { AuthProvider } from "./Context/AuthContext";
+import { getUserInfo } from "./utils/storage";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 import PublicRoutes from "./components/PublicRoutes";
-import { AuthProvider } from "./Context/AuthContext";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = React.useState(() =>
     getIsAuthenticated()
   );
+  const [userInfo, setUserInfo] = React.useState(async() => isAuthenticated ? await getUserInfo() : null);
+
+  React.useEffect(() => {
+    if(isAuthenticated) {
+      (async() => {
+        const userInfo = await getUserInfo()
+        setUserInfo(userInfo);
+      })()
+    }
+  }, [isAuthenticated, setUserInfo]);
 
   return (
     <Router>
       <ApolloProvider client={isAuthenticated ? client : authClient}>
-        <AuthProvider value={{ isAuthenticated, setIsAuthenticated }}>
+        <AuthProvider value={{ userInfo, isAuthenticated, setIsAuthenticated }}>
           {isAuthenticated ? <ProtectedRoutes /> : <PublicRoutes />}
         </AuthProvider>
       </ApolloProvider>
