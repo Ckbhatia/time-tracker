7760d162cda2524ae78e44978ef6d18aba667538
Ckbhatia <chetansain86@gmail.com>
2022-03-02 17:55:46 +0530

    feat: create saveInfo

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -6,6 +6,12 @@ export const getUserInfo = async () => {
   return await JSON.parse(localStorage.getItem(USER_INFO_TEXT));
 };
 
+export const saveInfo = (key, payload) => {
+  if(typeof key === 'string' && key && payload) {
+    return localStorage.setItem(key, JSON.stringify(payload));
+  }
+}
+
 export const getAuthToken = async () => {
   const userInfo = await JSON.parse(localStorage.getItem('userInfo'));
   return get(userInfo, 'token');
