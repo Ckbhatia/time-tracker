f1907d577b3d6e7ba681ecbe469e26cf09b895ca
Ckbhatia <chetansain86@gmail.com>
2022-03-05 23:03:48 +0530

    feat: create constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,19 @@
 // TASK SHOW
 export const DEFAULT_LIMIT = 10;
 
+//
+export const CREATE_NEW_TEXT =  "__createNew";
+
 // Storage
 export const USER_INFO_TEXT = 'userInfo';
+
+// Status
+export const SUCCESS_MESSAGE = 'Request successful';
+export const ERROR_MESSAGE = 'Something went wrong';
+
+// Toast
+export const DEFAULT_TOAST_DURATION = 2000;
+
+// Status
+export const ERROR_TEXT = 'error';
+export const SUCCESS_TEXT = 'success';
