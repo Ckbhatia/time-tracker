da4cbb0a7e2ec493590394f4b3e335d4ca301e5d
Ckbhatia <chetansain86@gmail.com>
2022-03-05 23:04:03 +0530

    feat: add tost

---
+++ src/utils/toast.js
@@ -0,0 +1,33 @@
+import { toast } from "react-toastify";
+import { DEFAULT_TOAST_DURATION } from '../constants';
+
+// Temp store descriptionQueue for ref
+const descriptionQueue = [];
+
+const tost = (
+  type,
+  description,
+  duration = DEFAULT_TOAST_DURATION,
+  position = "top-right",
+) => {
+  if (!descriptionQueue.includes(description)) {
+    toast[type](description, {
+      position,
+      autoClose: duration,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: true,
+      onClose: () => {
+        // Find & remove the message from messageQueue
+        const index = descriptionQueue.indexOf(description);
+        descriptionQueue.splice(index, 1);
+      },
+    });
+    // Push to messageQueue
+    descriptionQueue.push(description);
+  }
+};
+
+export default tost;
