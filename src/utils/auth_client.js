665848b695075483ccf90d6064ede090eadbdd8f
Ckbhatia <chetansain86@gmail.com>
2022-03-06 18:13:56 +0530

    fix: remove headers

--- src/utils/auth_client.js
+++ src/utils/auth_client.js
@@ -2,9 +2,6 @@ import ApolloClient from "apollo-boost";
 
 const authClient = new ApolloClient({
   uri: process.env.REACT_APP_BE_AUTH_URI,
-  headers: {
-    "x-hasura-admin-secret": process.env.REACT_APP_HASURA_ADMIN_SECRET,
-  },
 });
 
 export default authClient;
