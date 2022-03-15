0835f0d8b629cc3ba4421f2bafb2738b5df1d469
Ckbhatia <chetansain86@gmail.com>
2022-03-15 09:58:16 +0530

    feat: replace apollo boost with @apollo/client

--- src/utils/auth_client.js
+++ src/utils/auth_client.js
@@ -1,7 +1,8 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const authClient = new ApolloClient({
   uri: process.env.REACT_APP_BE_AUTH_URI,
+  cache: new InMemoryCache()
 });
 
 export default authClient;
--- src/utils/client.js
+++ src/utils/client.js
@@ -1,16 +1,24 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from '@apollo/client';
 import { getAuthToken } from "./storage";
 
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_BE_URI });
+
+const authLink = new ApolloLink(async (operation, forward) => {
+
+  const token = await getAuthToken();
+
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  });
+
+  return forward(operation);
+});
+
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_BE_URI,
-  request: async (operation) => {
-    const token = await getAuthToken();
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    });
-  },
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
 
 export default client;
