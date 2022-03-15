b3996c644141074b9289d29288790391b77cc651
Ckbhatia <chetansain86@gmail.com>
2022-03-15 09:58:50 +0530

    fix: replace graphql-tag with @apollo/client

--- src/service/tag.js
+++ src/service/tag.js
@@ -1,4 +1,4 @@
-import gql from "graphql-tag";
+import { gql } from '@apollo/client';
 
 export const GetTags = gql`
   query($author_id: Int!) {
--- src/service/task.js
+++ src/service/task.js
@@ -1,4 +1,4 @@
-import gql from "graphql-tag";
+import { gql } from '@apollo/client';
 
 export const GetTasks = gql`
 	query ($limit: Int!, $offset: Int!, $author_id: Int!) {
