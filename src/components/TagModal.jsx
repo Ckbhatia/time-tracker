19de404f23f6cbb745bd60dfc635120d074efb9f
Ckbhatia <chetansain86@gmail.com>
2025-09-08 20:26:48 +0530

    fix: separate TagModal

---
+++ src/components/TagModal.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Modal from "@material-ui/core/Modal";
+import TagForm from "./TagForm";
+
+export default function TagModal(props) {
+  const { open, setOpen, refetch, autoSelectTag } = props;
+
+  const handleClose = () => {
+    autoSelectTag();
+    setOpen(false);
+  };
+
+  const handleSave = (tagTitle, tagId) => {
+    autoSelectTag(tagTitle, tagId);
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <TagForm refetch={refetch} handleSave={handleSave} />
+      </Modal>
+    </div>
+  );
+}
